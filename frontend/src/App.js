a4d416b21bd7256b2bde51c6dea08418f08ef670
YuSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2025-04-20 16:04:07 +0900

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import MapPage from './pages/MapPage';
-import PromotionListPage from './pages/PromotionListPage';
-import PromotionCreatePage from './pages/PromotionCreatePage';
-import PromotionEditPage from './pages/PromotionEditPage';
-import PromotionPage from './pages/PromotionPage';
+import MapPage from './pages/Map/MapPage';
+import PromotionListPage from './pages/Promotion/PromotionListPage';
+import PromotionCreatePage from './pages/Promotion/PromotionCreatePage';
+import PromotionEditPage from './pages/Promotion/PromotionEditPage';
+import PromotionPage from './pages/Promotion/PromotionPage';
+import BoardListPage from './pages/Board/BoardListPage';
+import BoardCreatePage from './pages/Board/BoardCreatePage';
+import BoardEditPage from './pages/Board/BoardEditPage';
+import BoardPage from './pages/Board/BoardPage';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
     return (
         <Router>
             <nav>
-                <Link to="/">지도</Link> | <Link to="/promotions">홍보 게시판</Link>
+                <Navbar />
             </nav>
             <Routes>
                 <Route path="/" element={<MapPage />} />
@@ -18,6 +23,10 @@ function App() {
                 <Route path="/promotions/new" element={<PromotionCreatePage />} />
                 <Route path="/promotions/edit/:id" element={<PromotionEditPage />} />
                 <Route path="/promotions/:id" element={<PromotionPage />} />
+                <Route path="/boards" element={<BoardListPage />} />
+                <Route path="/boards/new" element={<BoardCreatePage />} />
+                <Route path="/boards/edit/:id" element={<BoardEditPage />} />
+                <Route path="/boards/:id" element={<BoardPage />} />
             </Routes>
         </Router>
     );
