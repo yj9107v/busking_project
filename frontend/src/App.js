7c96cd99dbb7ff72658fdc45ebf33bbc57f0862e
YuSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2025-04-24 17:30:10 +0900

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import MapPage from './pages/Map/MapPage';
 import PromotionListPage from './pages/Promotion/PromotionListPage';
 import PromotionCreatePage from './pages/Promotion/PromotionCreatePage';
