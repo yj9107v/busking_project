04bef6b5dd027090421f7eae50949f148e4d06e7
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-05-25 12:05:38 +0900

    Delete backend directory
    현 디렉토리 삭제
    
    새로운 디렉토리 생성 및  파일 정리를 통한 프로젝트 가독성 향상

--- backend/maxwell_busking/member_management/src/main/java/busking/busking_project/AuthProvider.java
+++
@@ -1,6 +0,0 @@
-package busking.busking_project;
-
-public enum AuthProvider {
-    LOCAL, GOOGLE, KAKAO
-}
-
--- backend/maxwell_busking/member_management/src/main/java/busking/busking_project/Role.java
+++
@@ -1,7 +0,0 @@
-package busking.busking_project;
-
-public enum Role {
-    USER,
-    ADMIN
-}
-
